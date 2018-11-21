bdfc2867e68cfd9c28c345bb6cebd547b0ec40f2
Patrick McMorris <pmcmorris@ea.com>
2018-11-20 20:38:12 -0800

    add option to control linker to use windows subsystem

--- bin/build.bat
+++ bin/build.bat
@@ -2,6 +2,8 @@
 
 set C_FLAGS=-Wall -pedantic -std=c99 -g
 set C_LIBS=
+::set C_LINK=-Wl,--subsystem,windows
+set C_LINK=-Wl,--subsystem,console
 
 :: path for the executable were building
 set output_path=%build_dir%\%target%%filetype_exe%
@@ -10,5 +12,5 @@ set output_path=%build_dir%\%target%%filetype_exe%
 md %build_dir% 2> NUL
 
 :: build the game
-gcc %C_FLAGS% %src_dir%\bulk.c -o %output_path% %C_LIBS%
+gcc %C_FLAGS% %src_dir%\bulk.c -o %output_path% %C_LIBS% %C_LINK%
 
